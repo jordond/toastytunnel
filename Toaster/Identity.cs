90ba9f39a4753485e9bfcb0d857f82e1e3a56d05
Jordon de Hoog <jordon.dehoog@gmail.com>
2014-06-11 09:39:16 -0400

    Added new library

---
+++ Toaster/Identity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toaster
+{
+    public class Identity
+    {
+        public int ID { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public bool Save { get; set; }
+
+        public Identity() { }
+
+        public Identity(int id, string user, string password, bool save)
+        {
+            ID = id;
+            User = user;
+            Password = password;
+            Save = save;
+        }
+
+        public List<Identity> loadIdentities()
+        {
+            return null;
+        }
+    }
+}
---
+++ Toaster/Session.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toaster
+{
+    public class Session
+    {
+        public int ID { get; set; }
+        public Identity identity { get; set; }
+        public string Host { get; set; }
+        public bool IsLocal { get; set; }
+        public int LocalPort { get; set; }
+        public string RemoteAddress { get; set; }
+        public int RemotePort { get; set; }
+
+        public Session() { }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public List<Session> loadSessions()
+        {
+            return null;
+        }
+    }
+}
