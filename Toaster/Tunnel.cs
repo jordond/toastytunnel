896c1200610593374c1127481afd51c3128d3cc8
Jordon de Hoog <jordon.dehoog@gmail.com>
2014-06-13 13:37:34 -0400

    added tunnelmanager

--- Toaster/Tunnel.cs
+++ Toaster/Tunnel.cs
@@ -10,44 +10,60 @@ namespace Toaster
 {
     public class Tunnel
     {
+        public int ID { get; set; }
         public string Name { get; set; }
+        public Identity identity { get; set; }
+        public string Host { get; set; }
+        public int LocalPort { get; set; }
+        public string RemoteAddress { get; set; }
+        public int RemotePort { get; set; }
+        public string ConnectionString { get; set; }        
+        public bool isOpen { get; set; }
+        public bool autoStart { get; set; }
         public Process Instance { get; set; }
-        public Session instanceSession { get; set; }
-        public string ConnectionString { get; set; }
-        public bool isStarted { get; set; }
-
         public ProcessStartInfo InstanceInfo
         {
-            get { return setStartInfo(); }
+            get 
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.FileName = Toast.plinkLocation;
+                info.Arguments = ConnectionString;
+                info.WindowStyle = ProcessWindowStyle.Minimized;
+                #if !DEBUG
+                temp.UseShellExecute = false;
+                temp.CreateNoWindow = true;
+                #endif
+                return info;
+            }
         }
 
-        public Tunnel(Session s)
+        public Tunnel()
         {
             Instance = new Process();
-            ID = -1;
-            instanceSession = s;
-            createTunnelName();
+            RemotePort = -1;
         }
-           
-        public void start()
+
+        //per tunnel start
+        public void Start()
         {
             try
             {
                 Instance.StartInfo = InstanceInfo;
                 Toast._logWriter.addEntry(LogLevels.INFO, "Digging the " + Name + " tunnel, with these specs: " + tunnelSpecs());
-                isStarted = Instance.Start();
+                isOpen = Instance.Start();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Toast._logWriter.addEntry(LogLevels.ERROR, "Digging the " + Name + " tunnel failed: " + ex.Message);
                 throw new Exception("Tunnel.cs - start() - " + ex.Message);
             }
         }
 
-        public void stop()
+        //per tunnel stop
+        public void Stop()
         {
             try
-            {                
+            {
                 if (Instance.HasExited)
                 {
                     Toast._logWriter.addEntry(LogLevels.WARNING, Name + " has already collapsed...");
@@ -57,52 +73,30 @@ namespace Toaster
                     Toast._logWriter.addEntry(LogLevels.INFO, "Collapsing the " + Name + " tunnel.");
                     Instance.Kill();
                 }
-                isStarted = false;
+                isOpen = false;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Toast._logWriter.addEntry(LogLevels.ERROR, "Could not collapse the " + Name + " tunnel: " + ex.Message);
                 throw new Exception("Tunnel.cs - stop() - " + ex.Message);
             }
         }
 
-        private void createTunnelName()
-        {
-            Name = instanceSession.identity.User + "@" + instanceSession.Host +
-                (instanceSession.IsLocal == false ? " D:" + instanceSession.RemotePort :
-                " L:" + instanceSession.LocalPort + ":" + instanceSession.RemoteAddress +
-                ":" + instanceSession.RemotePort);
-        }
-
-        private ProcessStartInfo setStartInfo()
-        {
-            ProcessStartInfo temp = new ProcessStartInfo();
-            temp.FileName = Toast.plinkLocation;
-            temp.Arguments = instanceSession.ConnectionString;
-            temp.WindowStyle = ProcessWindowStyle.Minimized;
-#if !DEBUG
-            temp.UseShellExecute = false;
-            temp.CreateNoWindow = true;
-#endif
-            return temp;
-        }
-
-        private string tunnelSpecs()
+        public string tunnelSpecs()
         {
             StringBuilder s = new StringBuilder();
             s.AppendLine("");
-            s.AppendLine("ID #: " + ID);
             s.AppendLine("Name: " + Name);
             s.AppendLine("Identity: ");
-            s.AppendLine("User: " + instanceSession.identity.User);
-            s.AppendLine("Host: " + instanceSession.Host);
-            s.AppendLine("Ports: " + (instanceSession.IsLocal == false ? " D:" + 
-                        instanceSession.RemotePort : " L:" + instanceSession.LocalPort + ":" + 
-                        instanceSession.RemoteAddress + ":" + instanceSession.RemotePort));
-            s.AppendLine(instanceSession.identity.PrivateKey == "" ? "Key: none" 
-                        : "Key: " + Path.GetFileName(instanceSession.identity.PrivateKey));
-            s.AppendLine("Password: " + instanceSession.identity.Password == "" ? "Password: none" : "Password: yes");
-            s.AppendLine("Quick Connect: " + instanceSession.identity.Save);
+            s.AppendLine("User: " + identity.User);
+            s.AppendLine("Host: " + Host);
+            s.AppendLine("Ports: " + (LocalPort == -1 ? " D:" +
+                        RemotePort : " L:" + LocalPort + ":" +
+                        RemoteAddress + ":" + RemotePort));
+            s.AppendLine(identity.PrivateKey == "" ? "Key: none"
+                        : "Key: " + Path.GetFileName(identity.PrivateKey));
+            s.AppendLine("Password: " + identity.Password == "" ? "Password: none" : "Password: yes");
+            s.AppendLine("Quick Connect: " + identity.Save);
             return s.ToString();
         }
     }
---
+++ Toaster/TunnelManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toaster
+{
+    public class TunnelManager
+    {
+        public List<Tunnel> Tunnels { get; set; }
+
+        public TunnelManager(List<Tunnel> t)
+        {
+            Tunnels = t;
+        }
+
+        #region Tunnel Start Methods
+        //autostart 
+        public void Start()
+        {
+            try
+            {
+                foreach (Tunnel t in Tunnels.Where(a => a.autoStart == true))
+                    t.Start();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("TunnelManager.cs Start() - " + ex.Message);
+            }
+        }
+
+        //start single
+        public void Start(int tunnelID)
+        {
+            try
+            {
+                Tunnels.First(t => t.ID == tunnelID).Start();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("TunnelManager.cs Start(int) - " + ex.Message);
+            }
+        }
+
+        //start multiple
+        public void Start(List<int> tunnelIDs)
+        {
+            try
+            {
+                foreach (int i in tunnelIDs)
+                {
+                    Tunnels.First(t => t.ID == i).Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("TunnelManager.cs Start(List<int>) - " + ex.Message);
+            }
+        }
+        #endregion
+
+        #region Tunnel Stop Methods
+        //stop all
+        public void Stop()
+        {
+            try
+            {
+                foreach (Tunnel t in Tunnels)
+                    t.Start();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("TunnelManager.cs Stop() - " + ex.Message);
+            }
+        }
+
+        //stop single
+        public void Stop(int tunnelID)
+        {
+            try
+            {
+                Tunnels.First(t => t.ID == tunnelID).Stop();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("TunnelManager.cs Stop(int) - " + ex.Message);
+            }
+        }
+
+        //stop multiple
+        public void Stop(List<int> tunnelIDs)
+        {
+            try
+            {
+                foreach (int i in tunnelIDs)
+                {
+                    Tunnels.First(t => t.ID == i).Stop();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("TunnelManager.cs Stop(List<int>) - " + ex.Message);
+            }
+        }
+        #endregion
+
+    }
+}
